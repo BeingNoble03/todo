6b4a5b45abe93aba3854bd42f03e6a81213aaa8f
Noble Mittal <62551163+BeingNoble03@users.noreply.github.com>
2022-01-14 01:41:34 +0530

    Add files via upload

---
+++ auth_required.js
@@ -0,0 +1,19 @@
+    const token = localStorage.getItem('token');
+    if (token === null) {
+        window.location.href = '/login';
+    } else {       
+    $.ajax({
+    headers: {
+        Authorization: 'Token ' + localStorage.getItem('token'),
+    },
+    url: API_BASE_URL + 'auth/profile/',
+    method: 'GET',
+    success: function(data, status, xhr) {
+          displaySuccessToast('You have been Authenticated.');
+    },
+    error: function(xhr, status, err){
+        window.location.href = '/login/index.html';
+    }
+    })
+    }
+console.log('hello');
